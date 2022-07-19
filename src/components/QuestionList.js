dba776dd8f38170e6b3cb58103937fbd6a585d3b
Jobsidney <jobsidney67@gmail.com>
2022-07-19 16:47:39 +0300

    Done with assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,22 +2,37 @@ import React,{useState,useEffect} from "react";
 
 function QuestionList() {
   const [Questions,setQuestions] =useState([])
+  const [count,setCount] = useState(0)
   useEffect(()=>{
     fetch('http://localhost:4000/questions')
     .then(response=>response.json())
     .then(data=>{
       setQuestions(data)
+      setCount(data.length)
       console.log(data);
+      console.log(count);
       
     })
-  },[])
+  },[count])
+  function deleteQuestion(id){
+    fetch('http://localhost:4000/questions/'+id,
+    {method: 'DELETE',
+    headers: {'Content-Type': 'application/json'},
+})
+if (count>0) {
+  setCount(count-1)
+}
+
+
+  }
 
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
         {Questions.map(question=>(
-          <li key={question.id}>{question.id}. {question.prompt}</li>
+          <li key={question.id}>{question.id}. {question.prompt} 
+          <button onClick={()=>deleteQuestion(question.id)}>Delete</button></li>
         ))}
       </ul>
     </section>
